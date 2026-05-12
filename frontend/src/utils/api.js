cf68c8b7d2aab0625a38c96d40757d4922e9f816
ShivanshSaxena03 <shivanshsaxena03102006@gmail.com>
2026-05-13 04:07:13 +0530

    api changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    'http://localhost:8000/api',
+    // 'http://localhost:8000/api',
     'https://studyforge-g7z7.onrender.com/api',
 })
 
