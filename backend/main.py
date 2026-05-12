fcffb41a251bf938956415a724709f6866c11dcf
ShivanshSaxena03 <shivanshsaxena03102006@gmail.com>
2026-05-13 02:15:07 +0530

    CORS

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="StudyForge API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://studyforge-backend.onrender.com/api","https://study-forge-phi.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
