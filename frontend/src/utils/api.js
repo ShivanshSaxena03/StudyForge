7238f663912785683932f28e22e0cb6205422ff1
ShivanshSaxena03 <shivanshsaxena03102006@gmail.com>
2026-05-13 10:51:56 +0530

    Token error fixed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,21 +1,5 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    'http://localhost:8000/api',
-    // 'https://studyforge-g7z7.onrender.com/api',
-})
-
-api.interceptors.request.use(config => {
-
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
-})
-
-export default api
+import axios from 'axios' 
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://studyforge-g7z7.onrender.com/api', }) 
+api.interceptors.request.use(config => { const token = localStorage.getItem('sf_token') 
+if (token) { config.headers.Authorization = `Bearer ${token}` } return config }) 
+  export default api
